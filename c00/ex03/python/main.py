4d249dd093784e16dc599ec1f307acd398c13fec
fab <fbarulligmail.com>
2023-03-08 19:35:10 +0100

    lol

---
+++ c00/ex03/python/main.py
@@ -0,0 +1,17 @@
+'''
+if __name__ == '__main__':
+	number = -1
+
+	if number >= 0:
+		print("P")
+	else:
+		print('N')
+'''
+
+def is_neg(num):
+	if num >= 0:
+		print("P")
+	else:
+		print('N')
+
+is_neg(-1)
