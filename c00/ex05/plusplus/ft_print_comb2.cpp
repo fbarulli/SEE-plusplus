05723a761ac990007dd9480ff1e35c2f79a79db5
fab <fbarulligmail.com>
2023-03-08 17:02:20 +0100

    lol

---
+++ c00/ex05/plusplus/ft_print_comb2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+void ft_putchar(int number)
+{
+  char c;
+  
+  if (number < 10)
+  {
+    c = number + 48;
+    std::cout.write("0", 1);
+    std::cout.write(&c, 1);
+    return ;    
+  }
+  c = (number/ 10) + 48;
+  std::cout.write(&c, 1);
+  c = (number % 10) + 48;
+  std::cout.write(&c, 1);
+}
+
+void ft_print_comb2(void)
+{
+  int array[2];
+  array[0] = 0;
+
+  while (array[0] <=98)
+  {  
+    
+    array[1] = array[0] + 1;
+    while (array[1] <= 99)
+    {
+      ft_putchar(array[0]);
+      std::cout.write(" ", 1);
+      ft_putchar(array[1]);
+      if (array[0] == 98 && array[1] == 99)
+        {
+          std::cout.write("", 2);
+        }
+      else 
+        std::cout.write(", ", 2);
+      array[1]++;   
+    }  
+    array[0]++;
+    
+        
+    }
+    
+}
+
+
+int main() {
+  ft_print_comb2();
+  
+}
