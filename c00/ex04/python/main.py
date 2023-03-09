7c2baea6ad5a07881f70a8e9756582d549505816
fab <fbarulligmail.com>
2023-03-09 16:05:08 +0100

    f strings doesnt work in replt

--- c00/ex04/python/main.py
+++ c00/ex04/python/main.py
@@ -1,27 +1,5 @@
-one = 0 
-two = 0
-three = 0
-''' 
-while (one <=7):
-	
-	while (two <= 8):
-		
-		while (three <= 9):
-			if (one == 7 & two == 8 & three == 9):
-				print(one, two, three, ", ")
-			else:
-				print(one,two,three)
-				
-		  three += 1
-  two = one + 1	
- one += 1
- if (one == 7 & two == 8 & three == 9):
-				print(one, two, three, end = ' ')
-	else:
-				print(one,two,three)	
-'''
 
-
+one = 0
 while (one <= 7):
 	two = one + 1
 	while (two <= 8):
