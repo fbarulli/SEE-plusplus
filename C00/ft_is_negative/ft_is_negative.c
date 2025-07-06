c157d26da496f3dd74f78f135cb4b5ab474c16fc
fabian <fbarulli@gmail.com>
2025-07-06 18:10:57 +0000

    combination

---
+++ C00/ft_is_negative/ft_is_negative.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void    ft_print_comb(void)
+{
+    char    digits[3];
+
+    digits[0] = '0';
+    while (digits[0] <= '7')  // First digit: 0 to 7
+    {
+        digits[1] = digits[0] + 1;
+        while (digits[1] <= '8')  // Second digit: (first+1) to 8
+        {
+            digits[2] = digits[1] + 1;
+            while (digits[2] <= '9')  // Third digit: (second+1) to 9
+            {
+                write(1, digits, 3);  // Print the combination
+                if (digits[0] != '7')  // Add comma and space if not last
+                    write(1, ", ", 2);
+                digits[2]++;
+            }
+            digits[1]++;
+        }
+        digits[0]++;
+    }
+}
