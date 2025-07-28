57216139f0fada06c3c2f6a7408f59be99a74de6
fbarulli <fbarulli@gmail.com>
2025-07-28 09:52:04 +0200

    more practice

--- C00/ft_comb_2/comb_2.c
+++ C00/ft_comb_2/comb_2.c
@@ -1,42 +1,42 @@
-
-
-#include <unistd.h>
-#include <stdio.h>
-
-
-
-
-// (condition) ? value_if_true : value_if_false;
-// ? is shorthand for if-else
 #include <unistd.h>
 
-#include <unistd.h>
-
-void    ft_print_comb2(void){
-    for (int i = 0; i <=98; i++){
-        for (int j = i + 1; j <=99; j++){
-            char num1[2] = { ( i / 10) + '0', (i % 10) + '0'};
-            char num2[2] = { ( j / 10) + '0', (j % 10) + '0'};
-            write(1, num1, 2);
-            write(1, " ", 1);
-            write(1, num2, 2);
-            if (i != 98 || j != 99)
-            write(1, ", ", 2);
-        }
-    }
 
 
 
-
-
-}
-
-    
-
-
-int main()
+void ft_putchar(char c)
 {
-    ft_print_comb2();
-
-    return 0;
+  write(1, &c, 1);
 }
+
+
+void ft_print_combo(void)
+{
+  for (char a = '0'; a <= '7'; a++)
+  {
+    for (char b = a+1; b<= '8'; b++)
+    {
+      for (char c = b + 1; c<='9'; c++)
+      {
+        ft_putchar(a);
+        ft_putchar(b);
+        ft_putchar(c);
+        if (!(a == '7' && b == '8' && c == '9'))
+        {
+          ft_putchar(',');
+          ft_putchar(' ');
+          
+        }
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+int main(){
+  ft_print_combo();
+} 
